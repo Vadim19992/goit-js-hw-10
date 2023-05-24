6c7c289ae4e9cef8301cea4909910153b7fb5ada
Oleksandr <workbov@gmail.com>
2023-05-24 08:08:33 +0200

    Add function Markup

--- src/index.js
+++ src/index.js
@@ -12,15 +12,24 @@ function updateCountryList(countries) {
     Notiflix.Notify.warning(
       'Too many matches found. Please enter a more specific name.'
     );
-  } else {
+  } else if (countries.length < 10 && countries.length > 2) {
     countries.forEach(country => {
       const listItem = document.createElement('li');
       listItem.classList.add('country-item');
       listItem.innerHTML = `
+      <h2><img src="${country.flags.svg}" alt="Flag" class="flag"> ${country.name.official}</h2>`;
+      countryList.appendChild(listItem);
+    });
+  } else {
+    countries.forEach(country => {
+      const listItem = document.createElement('li');
+      listItem.classList.add('country-item');
+      const languages = Object.values(country.languages).join(', ');
+      listItem.innerHTML = `
       <h2><img src="${country.flags.svg}" alt="Flag" class="flag"> ${country.name.official}</h2>
       <p><strong>Capital:</strong> ${country.capital}</p>
       <p><strong>Population:</strong> ${country.population}</p>
-      <p><strong>Languages:</strong> ${country.languages}</p>`;
+      <p><strong>Languages:</strong> ${languages}</p>`;
       countryList.appendChild(listItem);
     });
   }
@@ -33,11 +42,14 @@ const debouncedSearch = debounce(() => {
     countryInfo.innerHTML = '';
     return;
   }
+
   fetchCountries(searchTerm)
     .then(countries => {
       updateCountryList(countries);
     })
     .catch(error => {
+      countryList.innerHTML = '';
+      countryInfo.innerHTML = '';
       Notiflix.Notify.failure('Oops, there is no country with that name');
     });
 }, 300);
